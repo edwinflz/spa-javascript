a5887c1e6b0251e615a46db2a572ccf6642e22e0
edwin95florez <edwin.95.florez@gmail.com>
2020-05-04 13:53:43 -0500

    fix: Route homme GitPage

--- src/templates/header.js
+++ src/templates/header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="header-main">
         <div class="header-logo">
             <h1>
-                <a href="/">
+                <a href="/spa-javascript/">
                     100tifi.co
                 </a>
             </h1>
